14df09a81cc9be6bf51cdff8abdfe57927d90066
truyenho95 <truyenho.webdev@gmail.com>
2020-03-31 21:15:47 -0500

    initial commit

---
+++ src/test/java/com/truyenhln/spring_sercurity/SpringSercurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truyenhln.spring_sercurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSercurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
